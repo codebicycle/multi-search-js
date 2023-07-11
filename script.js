8db5b8921a02747321f750af44f463a8e9a22c93
codebicycle <codebicycle@gmail.com>
2023-07-11 10:55:53 +0300

    Add tags

--- script.js
+++ script.js
@@ -1,39 +1,76 @@
-const stores = {
-  "1": {"name": "dedeman.ro", "query": (searchTerm) => `https://beta.dedeman.ro/catalogsearch/result/v2/?q=${searchTerm}`},
-  "2": {"name": "leroymerlin.ro", "query": (searchTerm) => `https://www.leroymerlin.ro/produse/search/${searchTerm}`},
-  "3": {"name": "bricodepot.ro", "query": (searchTerm) => `https://www.bricodepot.ro/catalogsearch/result/?q=${searchTerm}`},
-  "4": {"name": "mathaus.ro", "query": (searchTerm) => `https://mathaus.ro/search?pb=true&text=${searchTerm}`},
-  "5": {"name": "hornbach.ro", "query": (searchTerm) => `https://www.hornbach.ro/s/${searchTerm}`},
-}
+const stores = [
+  {"name": "dedeman.ro", "query": (searchTerm) => `https://www.dedeman.ro/ro/catalogsearch/result/v2?q=${searchTerm}`, "tags": ["DIY"]},
+  {"name": "leroymerlin.ro", "query": (searchTerm) => `https://www.leroymerlin.ro/produse/search/${searchTerm}`, "tags": ["DIY"]},
+  {"name": "bricodepot.ro", "query": (searchTerm) => `https://www.bricodepot.ro/catalogsearch/result/?q=${searchTerm}`, "tags": ["DIY"]},
+  {"name": "mathaus.ro", "query": (searchTerm) => `https://mathaus.ro/search?pb=true&text=${searchTerm}`, "tags": ["DIY"]},
+  {"name": "hornbach.ro", "query": (searchTerm) => `https://www.hornbach.ro/s/${searchTerm}`, "tags": ["DIY"]},
+]
 
-const searchBox = document.getElementById('search')
+
+const searchBox = document.getElementById('search-box')
 
 
 function changeURL(event) {
   let searchTerm = encodeURI(searchBox.value)
-  id = event.target.id
-  queryFunc = stores[id]["query"]
+  let id = event.target.dataset.id
+  let queryFunc = stores[id]["query"]
   event.target.href = queryFunc(searchTerm)
 }
 
 
 function populate(stores) {
-  ul = document.getElementById("stores")
-  for (let id in stores) {
-    let linkText = document.createTextNode(stores[id]["name"])
+  pushIds(stores)
+  let tags = storesToTags(stores)
 
-    let a = document.createElement('a')
-    a.id = id
-    a.href = '#'
-    a.appendChild(linkText)
+  for (let tag in tags) {
+    let tagText = document.createTextNode(tag)
+    let h2 = document.createElement('h2')
+    h2.appendChild(tagText)
+    let ul = document.createElement('ul')
+    
+    for (let store of tags[tag]) {
+      let linkText = document.createTextNode(store["name"])
 
-    a.addEventListener('click', changeURL, {'capture': true})
+      let a = document.createElement('a')
+      a.dataset.id = store["id"]
+      a.href = '#'
+      a.appendChild(linkText)
 
-    let li = document.createElement('li')
-    li.appendChild(a)
+      a.addEventListener('click', changeURL, {'capture': true})
 
-    ul.appendChild(li)
+      let li = document.createElement('li')
+      li.appendChild(a)
+
+      ul.appendChild(li)
+    }
+
+    let storesBox = document.getElementById("stores-box")
+    storesBox.appendChild(h2)
+    storesBox.appendChild(ul)
   }
 }
 
+function storesToTags(stores) {
+  // Transpose the stores list to an object with tags as keys
+  let tags = {}
+  for (let store of stores) {
+    for (let tag of store["tags"]) {
+      if (tag in tags) {
+        tags[tag].push(store)
+      } else {
+        tags[tag] = [store]
+      }
+    }
+  }
+  return tags
+}
+
+function pushIds(list) {
+  // Add the array index to the composing objects as 'id'
+  for (let [index, el] of list.entries()) {
+    el['id'] = index.toString()
+  }
+}
+
+// main
 populate(stores)
