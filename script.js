b8a88c55ab41adeb35d4d7579f91155245dc304f
codebicycle <codebicycle@gmail.com>
2024-03-04 08:03:30 +0200

    Add used book shops

--- script.js
+++ script.js
@@ -13,20 +13,40 @@ const stores = [
 
   { "name": "emag", "query": (keyword) => `https://www.emag.ro/search/${keyword}`, "tags": ["e-store"] },
   { "name": "altex", "query": (keyword) => `https://altex.ro/cauta/?q=${keyword}`, "tags": ["e-store"] },
-  
+
   { "name": "hotukdeals", "query": (keyword) => `https://www.hotukdeals.com/search?q=${keyword}`, "tags": ["deals"] },
   { "name": "mydealz", "query": (keyword) => `https://www.mydealz.de/search?q=${keyword}`, "tags": ["deals"] },
-  
+
   { "name": "hotukdeals travel", "query": (keyword) => `https://www.hotukdeals.com/tag/travel`, "tags": ["travel deals"] },
   { "name": "mydealz travel", "query": (keyword) => `https://www.mydealz.de/gruppe/reisen`, "tags": ["travel deals"] },
+
+  { "name": "printre carti", "query": (keyword) => `https://www.printrecarti.ro/?cauta=${keyword}`, "tags": ["anticariat"] },
+  { "name": "targul cartii", "query": (keyword) => `https://www.targulcartii.ro/cauta/${keyword}`, "transform": slugify, "tags": ["anticariat"] },
+  { "name": "anticexlibris", "query": (keyword) => `https://www.anticexlibris.ro/produse/categorie?cid=-1&q=${keyword}`, "tags": ["anticariat"] },
+  { "name": "anticariat-doamnei", "query": (keyword) => `https://anticariat-doamnei.com/search/?q=${keyword}`, "tags": ["anticariat"] },
+  { "name": "anticariat-ursu", "query": (keyword) => `https://anticariat-ursu.ro/index.php?route=product/search&search=${keyword}`, "tags": ["anticariat"] },
+  { "name": "anticariat.net", "query": (keyword) => `https://www.anticariat.net/cautare-rezultate.php?t=${keyword}`, "tags": ["anticariat"] },
+  { "name": "casa literelor", "query": (keyword) => `https://www.casaliterelor.ro/searchanise/result?q=${keyword}`, "tags": ["anticariat"] },
+  { "name": "anticariat-unu", "query": (keyword) => `https://www.anticariat-unu.ro/cautare?keyword=${keyword}`, "tags": ["anticariat"] },
+  { "name": "carti-online", "query": (keyword) => `https://www.carti-online.ro/?s=${keyword}&search_id=product&post_type=product`, "tags": ["anticariat"] },
 ]
 
 const searchBox = document.getElementById('search-box')
 
+function slugify(text) {
+    return text.toString().toLowerCase()
+        .replace(/\s+/g, '-')           // Replace spaces with -
+        .replace(/[^\w-]+/g, '')        // Remove all non-word chars
+        .replace(/--+/g, '-')           // Replace multiple - with single -
+        .replace(/^-+/, '')             // Trim - from start of text
+        .replace(/-+$/, '')             // Trim - from end of text
+    }
+
 function changeURL(event) {
-  let keyword = encodeURI(searchBox.value)
   let id = event.target.dataset.id
   let queryFunc = stores[id]["query"]
+  let transform = stores[id]["transform"] || ((x) => x)
+  let keyword = encodeURI(transform(searchBox.value))
   event.target.href = queryFunc(keyword)
 }
 
